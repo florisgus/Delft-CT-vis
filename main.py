174ffed234fdf3eb4e8be7dc220d4dabcc6dfdca
florisgus <ftegussinklo@gmail.com>
2026-05-01 18:58:34 +0200

    added

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from delft-ct-vis!")
+from funcs.MakeVolume import MakeGrid, GetVolume
+from pyvista import wrap, Plotter
+import numpy as np
+
+Path = "./DELGT01-C2-15"
+Volume = GetVolume(Path, vocal=True)
+downsample_factor = 4
+if downsample_factor > 1:
+    Volume = Volume[
+        ::downsample_factor,
+        ::downsample_factor,
+        ::downsample_factor,
+    ]
+    print(f"Downsampled volume by factor {downsample_factor}")
 
 
-if __name__ == "__main__":
-    main()
+grid = wrap(Volume)
+grid = grid.extract_values(ranges=[1e3, np.inf], include_cells=True)
+edges = grid.extract_feature_edges(progress_bar=True)
+# edges.plot()
+plt = Plotter()
+# plt.add_mesh(mesh=edges, cmap="bone")
+# plt.add_volume(edges, cmap="bone")
+plt.add_mesh_clip_plane(grid, normal="-y", cmap="bone")
+plt.add_mesh_clip_plane(edges, normal="-y", color="red")
+plt.add_axes()
+plt.show()
